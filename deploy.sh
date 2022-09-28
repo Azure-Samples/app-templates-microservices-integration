0967ba960a003cd771b8aa90f0b328d87648e435
Alfero Chingono <2288092+achingono@users.noreply.github.com>
2022-09-28 18:57:01 +0000

    Create registry first and deploy images before provisioning infra #13

--- deploy.sh
+++ deploy.sh
@@ -17,15 +17,27 @@ fi
 SUFFIX=reddog-$CODE
 REGISTRY=acrreddog$CODE
 
-# provision infrastructure
-az deployment sub create --location $LOCATION --template-file ./infra/main.bicep --parameters name=$NAME --parameters location=$LOCATION --parameters uniqueSuffix=$SUFFIX
+################################################## 
+### CREATE REGISTRY
+##################################################
 
+# create resource group
+az group create --name ${NAME}-${SUFFIX} --location $LOCATION
+
+# create container registry
+az acr create --resource-group ${NAME}-${SUFFIX} --name $REGISTRY --sku Basic --admin-enabled true
+
+# retrieve registry credentials
 REGISTRY_URL=https://${REGISTRY}.azurecr.io
 REGISTRY_USERNAME=$(az acr credential show --name $REGISTRY --query username | xargs)
 REGISTRY_PASSWORD=$(az acr credential show --name $REGISTRY --query passwords[0].value | xargs)
 
 # build solution
-cd ./src && dotnet restore && dotnet build && cd ../
+# cd ./src && dotnet restore && dotnet build && cd ../
+
+################################################## 
+### BUILD IMAGES
+##################################################
 
 # build accounting service
 az acr build --image reddog/accounting-service:{{.Run.ID}} --image reddog/accounting-service:latest --registry $REGISTRY -f ./src/RedDog.AccountingService/Dockerfile ./src
@@ -57,32 +69,43 @@ az acr build --image reddog/virtual-customer:{{.Run.ID}} --image reddog/virtual-
 # build virtual worker
 az acr build --image reddog/virtual-worker:{{.Run.ID}} --image reddog/virtual-worker:latest --registry $REGISTRY -f ./src/RedDog.VirtualWorker/Dockerfile ./src
 
+################################################## 
+### PROVISION INFRASTRUCTURE
+##################################################
+
+# provision infrastructure
+az deployment sub create --location $LOCATION --template-file ./infra/main.bicep --parameters name=$NAME --parameters location=$LOCATION --parameters uniqueSuffix=$SUFFIX
+
+################################################## 
+### DEPLOY IMAGES
+##################################################
+
 # deploy accounting service image
-az containerapp up --name accounting-service --resource-group ${NAME}-${SUFFIX} --image ${REGISTRY}.azurecr.io/reddog/accounting-service:latest
+# az containerapp up --name accounting-service --resource-group ${NAME}-${SUFFIX} --image ${REGISTRY}.azurecr.io/reddog/accounting-service:latest
 
 # deploy bootstrapper image
-az containerapp up --name bootstrapper --resource-group ${NAME}-${SUFFIX} --image ${REGISTRY}.azurecr.io/reddog/bootstrapper:latest
+# az containerapp up --name bootstrapper --resource-group ${NAME}-${SUFFIX} --image ${REGISTRY}.azurecr.io/reddog/bootstrapper:latest
 
 # deploy corporate transfer service image
-az containerapp up --name corporate-transfer --resource-group ${NAME}-${SUFFIX} --image ${REGISTRY}.azurecr.io/reddog/corporate-transfer:latest
+# az containerapp up --name corporate-transfer --resource-group ${NAME}-${SUFFIX} --image ${REGISTRY}.azurecr.io/reddog/corporate-transfer:latest
 
 # deploy loyalty service image
-az containerapp up --name loyalty-service --resource-group ${NAME}-${SUFFIX} --image ${REGISTRY}.azurecr.io/reddog/loyalty-service:latest
+# az containerapp up --name loyalty-service --resource-group ${NAME}-${SUFFIX} --image ${REGISTRY}.azurecr.io/reddog/loyalty-service:latest
 
 # deploy makeline service image
-az containerapp up --name make-line-service --resource-group ${NAME}-${SUFFIX} --image ${REGISTRY}.azurecr.io/reddog/make-line:latest
+# az containerapp up --name make-line-service --resource-group ${NAME}-${SUFFIX} --image ${REGISTRY}.azurecr.io/reddog/make-line:latest
 
 # deploy order service image
-az containerapp up --name order-service --resource-group ${NAME}-${SUFFIX} --image ${REGISTRY}.azurecr.io/reddog/order-service:latest
+# az containerapp up --name order-service --resource-group ${NAME}-${SUFFIX} --image ${REGISTRY}.azurecr.io/reddog/order-service:latest
 
 # deploy receipt service image
-az containerapp up --name receipt-service --resource-group ${NAME}-${SUFFIX} --image ${REGISTRY}.azurecr.io/reddog/receipt-service:latest
+# az containerapp up --name receipt-service --resource-group ${NAME}-${SUFFIX} --image ${REGISTRY}.azurecr.io/reddog/receipt-service:latest
 
 # deploy UI image
-az webapp config container set --name ui-${SUFFIX} --resource-group ${NAME}-${SUFFIX} --docker-custom-image-name ${REGISTRY}.azurecr.io/reddog/ui:latest --docker-registry-server-url $REGISTRY_URL --docker-registry-server-user $REGISTRY_USERNAME --docker-registry-server-password $REGISTRY_PASSWORD
+# az webapp config container set --name ui-${SUFFIX} --resource-group ${NAME}-${SUFFIX} --docker-custom-image-name ${REGISTRY}.azurecr.io/reddog/ui:latest --docker-registry-server-url $REGISTRY_URL --docker-registry-server-user $REGISTRY_USERNAME --docker-registry-server-password $REGISTRY_PASSWORD
 
 # deploy Virtual Customer image
-az functionapp config container set --name vc-${SUFFIX} --resource-group ${NAME}-${SUFFIX} --docker-custom-image-name ${REGISTRY}.azurecr.io/reddog/virtual-customer:latest --docker-registry-server-url $REGISTRY_URL --docker-registry-server-user $REGISTRY_USERNAME --docker-registry-server-password $REGISTRY_PASSWORD
+# az functionapp config container set --name vc-${SUFFIX} --resource-group ${NAME}-${SUFFIX} --docker-custom-image-name ${REGISTRY}.azurecr.io/reddog/virtual-customer:latest --docker-registry-server-url $REGISTRY_URL --docker-registry-server-user $REGISTRY_USERNAME --docker-registry-server-password $REGISTRY_PASSWORD
 
 # deploy virtual worker image
-az containerapp up --name virtual-worker --resource-group ${NAME}-${SUFFIX} --image ${REGISTRY}.azurecr.io/reddog/virtual-worker:latest 
+# az containerapp up --name virtual-worker --resource-group ${NAME}-${SUFFIX} --image ${REGISTRY}.azurecr.io/reddog/virtual-worker:latest 
